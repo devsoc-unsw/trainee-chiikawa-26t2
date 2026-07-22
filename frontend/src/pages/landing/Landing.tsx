d4b6446c591fb52eb99b759f5ad1a7b7e7503f74
Hanwen <hanwen@DESKTOP-KNUSI9E.localdomain>
2026-07-22 19:09:41 +1000

    feat: Added landing page

--- frontend/src/pages/landing/Landing.tsx
+++ frontend/src/pages/landing/Landing.tsx
@@ -1,3 +1,21 @@
+import styles from "./landing.module.css"
+import { useState } from "react"
 export default function Landing() {
-  return <p>Hello world!</p>
+  const [clicked, setClicked] = useState(false);
+
+  return <div>
+    <img src="/learnternBackground.png" className={styles.background}></img>
+    <div className={!clicked ? styles.darkness : `${styles.darkness} ${styles.panIn}`}></div>
+    <div className={styles.main}>
+      <div className={styles.titleWrapper}>
+        <div className={!clicked ? styles.candleLight : `${styles.candleLight} ${styles.out}`}>
+          <div className={!clicked ? styles.candle : `${styles.candle} ${styles.blowOut}`}></div>
+        </div>
+        <div className={styles.title}>earntern</div>
+      </div>
+      <div className={styles.bio}> Illuminate your learning. </div>
+      <div className={styles.startButton} onClick={() => setClicked(true)}></div>
+    </div>
+
+  </div>
 }
