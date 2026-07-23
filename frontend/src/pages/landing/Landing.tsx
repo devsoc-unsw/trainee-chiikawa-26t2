3438d6dd5990bc92136771bdc4b52c1bd25b70aa
Hanwen Xiao <mailhanwenx@gmail.com>
2026-07-23 17:32:38 +1000

    feat: added audio

--- frontend/src/pages/landing/Landing.tsx
+++ frontend/src/pages/landing/Landing.tsx
@@ -1,11 +1,28 @@
 import styles from "./landing.module.css"
-import { useState } from "react"
+import { useRef, useState } from "react"
+
 export default function Landing() {
+
   const [clicked, setClicked] = useState(false);
+  const [mute, setMute] = useState(true);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  function toggleMute() {
+    if (!audioRef.current) return;
+
+    if (mute) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+    setMute(!mute);
+  }
 
   return <div>
+    <audio ref={audioRef} src="/Learntern Theme.mp3" loop />
     <img src="/learnternBackground.png" className={styles.background}></img>
     <div className={!clicked ? styles.darkness : `${styles.darkness} ${styles.panIn}`}></div>
+    <div onClick={toggleMute} className={!mute ? styles.soundButton : `${styles.soundButton} ${styles.muted}`}></div>
     <div className={styles.main}>
       <div className={styles.titleWrapper}>
         <div className={!clicked ? styles.candleLight : `${styles.candleLight} ${styles.out}`}>
